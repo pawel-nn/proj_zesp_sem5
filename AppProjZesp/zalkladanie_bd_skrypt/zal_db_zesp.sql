b2d73aa07af2935afa49f586e6cd91895ba89fa8
komet22 <pawelm642@onet.pl>
2017-06-12 01:21:59 +0200

    Drobne poprawki po merge

--- AppProjZesp/zalkladanie_bd_skrypt/zal_db_zesp.sql
+++ AppProjZesp/zalkladanie_bd_skrypt/zal_db_zesp.sql
@@ -19,18 +19,24 @@ insert into spring_users (username, password, enabled) values ('adm','adm1', 1);
 insert into spring_user_roles (username, role) values ('adm','ROLE_ADMIN');
 insert into spring_users (username, password, enabled) values ('emp','emp1', 1);
 insert into spring_user_roles (username, role) values ('emp','ROLE_EMPLOYEE');
+insert into spring_users (username, password, enabled) values ('emp2','emp1', 1);
+insert into spring_user_roles (username, role) values ('emp2','ROLE_EMPLOYEE');
 
 /* Klient 1 employee 1 musz¹ istniec */
 /* po odpaleniu spring jak istnieja inne tabele*/
 /*
 insert into client_accounts_web (username, password, enabled, session_id) values ('alama','alama', 1, null);
 insert into clients (client_id, accommodation_number, city, city_post_code, email, first_name, last_name, mobile, street, username) values (1,'23', "Miato 1", "20-200", "rrt@ffr.cd", "Ala", "Ma", "6775443567", "strasse 1", "alama");
-insert into employees (employee_id, accommodation_number, city, city_post_code, email, first_name, last_name, mobile, position, salary, street, username) values (1,'23', "Miato 1", "20-200", "rrt@ffr.cd", "Ada", "Kota", "1176453", "Klucznik", "1z³", "strasse 2", "emp");
+insert into employees (employee_id, accommodation_number, city, city_post_code, email, first_name, last_name, mobile, position, salary, street, username, path_to_profile_photo, profile_description) values (1,'23', "Miato 1", "20-200", "rrt@ffr.cd", "Ada", "Kota", "1176453", "Klucznik", "1z³", "strasse 2", "emp", null, null);
+insert into employees (employee_id, accommodation_number, city, city_post_code, email, first_name, last_name, mobile, position, salary, street, username, path_to_profile_photo, profile_description) values (2,'31', "Miato 2", "20-220", "rrt@feefr.cd", "Cytryna", "Szafa", "117645223", "PoteznyBiznesmen", "19999z³", "strasse 3", "emp2", null, null);
 insert  into cooperations (cooperation_id, subject, date_of_last_event, type_of_cooperation, client_id, employee_id, description) values (1, "eee", null, "Sprawa s¹dowa", 1, 1, "EEEeee1");
 insert  into cooperations (cooperation_id, subject, date_of_last_event, type_of_cooperation, client_id, employee_id, description) values (2, "yyy", null, "Doradztwo prawne", 1, 1, "EEEeee2");
 
 insert into client_accounts_web (username, password, enabled) values ('ttt','ttt', 1);
 insert into clients (client_id, accommodation_number, city, city_post_code, email, first_name, last_name, mobile, street, username) values (0, '23', "ttt", "20-200", "ttt@ffr.cd", "ttt", "ttt", "6775443567", "strasse 1", "ttt");
 
-select * from clients
+select * from cooperation_offers;
+
+select * from clients;
+select * from employees;
 */
